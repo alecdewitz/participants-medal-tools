11e4feb432e17db9f789a3c442fea0d6dea90a80
Alec Dewitz <atdewitz@comcast.net>
2019-03-23 20:39:57 -0700

    added user authentication to github

--- run-sql-query.php
+++ run-sql-query.php
@@ -4,12 +4,11 @@
  * Plugin Name:       Run SQL Query
  * Plugin URI:        https://github.com/alecdewitz/run-sql-query
  * Description:       Run SQL queries on your WordPress database, to operate use the plugin page under Tools &gt; <a href="tools.php?page=run-sql-query">Run SQL Query</a> to operate.
- * Version:           1.0.26
+ * Version:           1.0.27
  * Author:            Alec Dewitz
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       run-sql-query
- * Network: True
  */
 
 // If this file is called directly, abort.
@@ -23,6 +22,8 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	__FILE__,
 	'run-sql-query'
 );
+
+$myUpdateChecker->setAuthentication('3be42bd898d3cb1418f2614eb953be3a0114321e');
 
 
 $myUpdateChecker->setBranch('master');
