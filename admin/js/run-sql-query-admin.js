3c08108b7ff2974488532d8eddc83ee23792ff31
Alec Dewitz <atdewitz@comcast.net>
2019-03-23 19:56:11 -0700

    add create table confirmation

--- admin/js/run-sql-query-admin.js
+++ admin/js/run-sql-query-admin.js
@@ -141,9 +141,7 @@
   }
 
   function resetDatabase() {
-    var resetConfirm = confirm(
-      'Are you sure you want to run the following query\n\n' + $('#query').val()
-    );
+    var resetConfirm = confirm('Are you sure you want to reset the database?');
 
     if (resetConfirm) {
       // drop table
@@ -156,36 +154,36 @@
         run_sql_query();
       }
 
-      // create table
-      $('#query').val(
-        'CREATE TABLE `' +
-          $('#participants_database_fields').val() +
-          "` (`id` int(3) NOT NULL AUTO_INCREMENT,`order` int(3) NOT NULL DEFAULT '0',`name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,`title` tinytext COLLATE utf8_unicode_ci NOT NULL,`default` tinytext COLLATE utf8_unicode_ci,`group` varchar(64) COLLATE utf8_unicode_ci NOT NULL,`form_element` tinytext COLLATE utf8_unicode_ci,`options` longtext COLLATE utf8_unicode_ci,`attributes` text COLLATE utf8_unicode_ci,`validation` tinytext COLLATE utf8_unicode_ci,`validation_message` text COLLATE utf8_unicode_ci,`help_text` text COLLATE utf8_unicode_ci,`display_column` int(3) DEFAULT '0',`admin_column` int(3) DEFAULT '0',`sortable` tinyint(1) DEFAULT '0',`CSV` tinyint(1) DEFAULT '0',`persistent` tinyint(1) DEFAULT '0',`signup` tinyint(1) DEFAULT '0',`readonly` tinyint(1) DEFAULT '0') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
-      );
-      if (check_sql_query()) {
-        run_sql_query();
-      }
+      resetConfirm = confirm('Are you sure you want to create the database?');
 
-      setTimeout(function() {}, 1000);
+      if (resetConfirm) {
+        // create table
+        $('#query').val(
+          'CREATE TABLE `' +
+            $('#participants_database_fields').val() +
+            "` (`id` int(3) NOT NULL AUTO_INCREMENT,`order` int(3) NOT NULL DEFAULT '0',`name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,`title` tinytext COLLATE utf8_unicode_ci NOT NULL,`default` tinytext COLLATE utf8_unicode_ci,`group` varchar(64) COLLATE utf8_unicode_ci NOT NULL,`form_element` tinytext COLLATE utf8_unicode_ci,`options` longtext COLLATE utf8_unicode_ci,`attributes` text COLLATE utf8_unicode_ci,`validation` tinytext COLLATE utf8_unicode_ci,`validation_message` text COLLATE utf8_unicode_ci,`help_text` text COLLATE utf8_unicode_ci,`display_column` int(3) DEFAULT '0',`admin_column` int(3) DEFAULT '0',`sortable` tinyint(1) DEFAULT '0',`CSV` tinyint(1) DEFAULT '0',`persistent` tinyint(1) DEFAULT '0',`signup` tinyint(1) DEFAULT '0',`readonly` tinyint(1) DEFAULT '0') ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
+        );
+        if (check_sql_query()) {
+          run_sql_query();
+        }
+      }
 
       // insert into table
-      $('#query').val(
-        'INSERT INTO `' +
-          $('#participants_database_fields').val() +
-          "` (`id`, `order`, `name`, `title`, `default`, `group`, `form_element`, `options`, `attributes`, `validation`, `validation_message`, `help_text`, `display_column`, `admin_column`, `sortable`, `CSV`, `persistent`, `signup`, `readonly`) VALUES(1, 1005, 'FIRST_NAME', 'First Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 3, 1, 1, 1, 0, 0, 0),(2, 1006, 'LAST_NAME', 'Last Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 4, 2, 1, 1, 0, 0, 0),(4, 1009, 'CITY', 'City', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 1, 1, 1, 0, 0),(5, 1010, 'REGION_NAME', 'Region', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 1, 1, 1, 0, 0),(6, 1011, 'COUNTRY_NAME', 'Country', NULL, 'main', 'text-line', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 1, 1, 0, 0),(9, 1012, 'EMAIL', 'Email', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'email-regex', '', '', 0, 3, 0, 1, 0, 0, 0),(15, 14, 'id', 'Record ID', '', '', '', 'a:0:{}', 'a:0:{}', '', '', '', 0, 0, 0, 0, 0, 0, 0),(16, 15, 'private_id', 'Private ID', 'RPNE2', 'internal', 'text', NULL, NULL, 'no', NULL, NULL, 0, 4, 0, 0, 0, 1, 1),(17, 16, 'date_recorded', 'Date Recorded', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 5, 1, 0, 0, 0, 1),(18, 17, 'date_updated', 'Date Updated', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 0, 0, 0, 1),(19, 18, 'last_accessed', 'Last Accessed', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 0, 0, 0, 1),(20, 1000, 'RACE', 'Race', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 1, 0, 0, 1, 0, 0, 0),(21, 1003, 'REG_CHOICE', 'Registration Choice', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 2, 0, 0, 1, 0, 0, 0),(22, 1007, 'GENDER', 'Gender', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 0, 1, 0, 0, 0),(23, 1008, 'DOB', 'Date of Birth', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 5, 0, 0, 1, 0, 0, 0),(24, 1004, 'RACE_NAME', 'Race Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 0, 0, 0, 0, 0, 0, 0),(25, 1002, 'EXTERNAL_ID', 'External ID', NULL, 'main', 'text-line', NULL, NULL, 'no', NULL, NULL, 0, 0, 0, 0, 0, 0, 0),(26, 1001, 'CHRONO_ID', 'Chrono ID', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 0, 0, 0, 0, 0, 0, 0);"
-      );
-      if (check_sql_query()) {
-        run_sql_query();
-      }
-
-      setTimeout(function() {}, 1000);
+      // $('#query').val(
+      //   'INSERT INTO `' +
+      //     $('#participants_database_fields').val() +
+      //     "` (`id`, `order`, `name`, `title`, `default`, `group`, `form_element`, `options`, `attributes`, `validation`, `validation_message`, `help_text`, `display_column`, `admin_column`, `sortable`, `CSV`, `persistent`, `signup`, `readonly`) VALUES(1, 1005, 'FIRST_NAME', 'First Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 3, 1, 1, 1, 0, 0, 0),(2, 1006, 'LAST_NAME', 'Last Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 4, 2, 1, 1, 0, 0, 0),(4, 1009, 'CITY', 'City', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 1, 1, 1, 0, 0),(5, 1010, 'REGION_NAME', 'Region', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 1, 1, 1, 0, 0),(6, 1011, 'COUNTRY_NAME', 'Country', NULL, 'main', 'text-line', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 1, 1, 0, 0),(9, 1012, 'EMAIL', 'Email', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'email-regex', '', '', 0, 3, 0, 1, 0, 0, 0),(15, 14, 'id', 'Record ID', '', '', '', 'a:0:{}', 'a:0:{}', '', '', '', 0, 0, 0, 0, 0, 0, 0),(16, 15, 'private_id', 'Private ID', 'RPNE2', 'internal', 'text', NULL, NULL, 'no', NULL, NULL, 0, 4, 0, 0, 0, 1, 1),(17, 16, 'date_recorded', 'Date Recorded', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 5, 1, 0, 0, 0, 1),(18, 17, 'date_updated', 'Date Updated', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 0, 0, 0, 1),(19, 18, 'last_accessed', 'Last Accessed', NULL, 'internal', 'timestamp', NULL, NULL, 'no', NULL, NULL, 0, 0, 1, 0, 0, 0, 1),(20, 1000, 'RACE', 'Race', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 1, 0, 0, 1, 0, 0, 0),(21, 1003, 'REG_CHOICE', 'Registration Choice', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 2, 0, 0, 1, 0, 0, 0),(22, 1007, 'GENDER', 'Gender', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 0, 0, 0, 1, 0, 0, 0),(23, 1008, 'DOB', 'Date of Birth', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'no', '', '', 5, 0, 0, 1, 0, 0, 0),(24, 1004, 'RACE_NAME', 'Race Name', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 0, 0, 0, 0, 0, 0, 0),(25, 1002, 'EXTERNAL_ID', 'External ID', NULL, 'main', 'text-line', NULL, NULL, 'no', NULL, NULL, 0, 0, 0, 0, 0, 0, 0),(26, 1001, 'CHRONO_ID', 'Chrono ID', '', 'main', 'text-line', 'a:0:{}', 'a:0:{}', 'yes', '', '', 0, 0, 0, 0, 0, 0, 0);"
+      // );
+      // if (check_sql_query()) {
+      //   run_sql_query();
+      // }
 
       // alter table
-      $('#query').val(
-        'ALTER TABLE `' +
-          $('#participants_database_fields').val() +
-          '` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`),ADD KEY `order` (`order`),ADD KEY `group` (`group`);'
-      );
+      // $('#query').val(
+      //   'ALTER TABLE `' +
+      //     $('#participants_database_fields').val() +
+      //     '` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `name` (`name`),ADD KEY `order` (`order`),ADD KEY `group` (`group`);'
+      // );
     }
   }
 
